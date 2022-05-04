150d3049323b5447cdcd2c80d6b1bca2a3a0abbf
Gyuris Csaba <gycsaba72@gmail.com>
2022-05-04 10:02:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,2 @@
+# Az adatok egy autó rendszámát, színét, forgalomba hozásának dátumát és az ülések számát tartalmazza
+adatok="AAA-111,zöld,2002,5;BBB-222,piros,2003,5;CCC-333,kék,2004,4;DDD-444,piros,2002,4;EEE-555,zöld,2003,7;FFF-666,piros,2005,5"
